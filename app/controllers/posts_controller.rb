8e495ebdc6ee53744ed2d38d9e872668486b86d4
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-16 23:27:49 +0100

    delete commented code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,63 +1,25 @@
 class PostsController < ApplicationController
   before_action :allowed?, only: [:new]
 
-  # GET /posts
-  # GET /posts.json
   def index
     @posts = Post.all
     @current_user = User.find_by(remember_token: cookies[:remember_me])
   end
 
-  # GET /posts/1
-  # GET /posts/1.json
-  def show; end
-
-  # GET /posts/new
   def new
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit; end
-
   def create
     @current_user = User.find_by(remember_token: cookies[:remember_me])
     @post = @current_user.posts.build(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to root_url, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to root_url, notice: 'Post was successfully created.' 
+    else
+      render :new 
     end
   end
 
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
 
@@ -66,7 +28,6 @@ class PostsController < ApplicationController
     redirect_to root_url unless @current_user
   end
 
-  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:title, :body, :user_id)
   end
